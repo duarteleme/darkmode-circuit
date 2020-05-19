76495cb10cb8667b9a1eacd4d363aba5eba5f16c
Fabio Duarte <duarteleme@gmail.com>
2020-05-18 21:49:35 -0300

    Insere condições para aplicar o Dark Mode

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-function addClass() {
-    var body = document.body;
-    body.classList.toggle("darkmode");
-}
+// function addClass() {
+//     var body = document.body;
+//     body.classList.toggle("darkmode");
+// }
+
+
+var statusDarkMode = true;
+var body = document.body;
+
+body.classList.add("darkmode");
 
 chrome.runtime.onMessage.addListener(function(request) {
-    if(request.action === 'executeCode') {
-        addClass();
+    if(request.action === 'executeCode') {        
+        if (statusDarkMode) {
+            body.classList.remove("darkmode");
+            statusDarkMode = false;
+        } else {
+            body.classList.add("darkmode");
+            statusDarkMode = true;
+        }
     }
 });
--- popup.js
+++ popup.js
@@ -1,15 +1,18 @@
 window.onload = function () {
     
-    function changeStatus() {
-        var element = document.getElementById("toggle-btn");
-        element.classList.toggle("active");
-    }
+    var statusDarkMode = true;
+    var element = document.getElementById("toggle-btn");
 
     document.getElementById('toggle-btn').addEventListener('click', function() {
         chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
-            changeStatus();
+            if (statusDarkMode) {
+                element.classList.remove("active");
+                statusDarkMode = false;
+            } else {
+                element.classList.add("active");
+                statusDarkMode = true;
+            }
             chrome.tabs.sendMessage(activeTabs[0].id, { action: 'executeCode' });
-            
         });
     });
 };
